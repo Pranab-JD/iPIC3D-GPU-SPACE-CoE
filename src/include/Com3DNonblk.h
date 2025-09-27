c1c24f40812075e1c58b59d204b63cedfed67fec
PJD <pranab.deka@kuleuven.be>
2025-09-28 01:22:04 +0300

    Added old communication routines for 4D arrays

--- src/include/Com3DNonblk.h
+++ src/include/Com3DNonblk.h
@@ -39,36 +39,55 @@ developers           : Stefano Markidis, Ivy Bo Peng
 #include "Alloc.h"
 #include "debug.h"
 #include "EMfields3D.h"
+#include "ComParser3D.h"
 
-/** communicate ghost cells (FOR NODES) */
-void communicateNodeBC(int nx, int ny, int nz, arr3_double vector, int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, int bcFaceZright, int bcFaceZleft, const VirtualTopology3D * vct, EMfields3D *EMf);
+//* Communicate ghost cells (nodes)
+void communicateNodeBC( int nx, int ny, int nz, arr3_double vector, 
+                        int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, int bcFaceZright, int bcFaceZleft, 
+                        const VirtualTopology3D * vct, EMfields3D *EMf);
 
-// PARTICLES
+void communicateNodeBC_old( int nx, int ny, int nz, double ***vector, 
+                            int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, int bcFaceZright, int bcFaceZleft, 
+                            const VirtualTopology3D *vct, EMfields3D *EMf);
+
+void communicateNodeBoxStencilBC(int nx, int ny, int nz, arr3_double vector, 
+                                int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, int bcFaceZright, int bcFaceZleft, 
+                                const VirtualTopology3D * vct, EMfields3D *EMf);
+
+void communicateNodeBoxStencilBC_P( int nx, int ny, int nz, arr3_double vector, 
+                                    int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, int bcFaceZright, int bcFaceZleft, 
+                                    const VirtualTopology3D * vct, EMfields3D *EMf);
+
+void communicateNodeBC_P(int nx, int ny, int nz, arr3_double vector, 
+                         int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, int bcFaceZright, int bcFaceZleft, 
+                         const VirtualTopology3D * vct, EMfields3D *EMf);
+
+void communicateCenterBC(int nx, int ny, int nz, arr3_double vector, 
+                         int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, int bcFaceZright, int bcFaceZleft, 
+                         const VirtualTopology3D * vct, EMfields3D *EMf);
+
+void communicateCenterBC_P( int nx, int ny, int nz, arr3_double vector, 
+                            int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, int bcFaceZright, int bcFaceZleft, 
+                            const VirtualTopology3D * vct, EMfields3D *EMf);
+
+void communicateCenterBoxStencilBC( int nx, int ny, int nz, arr3_double vector, 
+                                    int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, int bcFaceZright, int bcFaceZleft, 
+                                    const VirtualTopology3D * vct, EMfields3D *EMf);
+
+void communicateCenterBoxStencilBC_P(int nx, int ny, int nz, arr3_double vector, 
+                                     int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, int bcFaceZright, int bcFaceZleft, 
+                                     const VirtualTopology3D * vct, EMfields3D *EMf);
+
 void communicateNode_P(int nx, int ny, int nz, double*** vector, const VirtualTopology3D * vct, EMfields3D *EMf);
 
-void communicateNodeBoxStencilBC(int nx, int ny, int nz, arr3_double vector, int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, int bcFaceZright, int bcFaceZleft, const VirtualTopology3D * vct, EMfields3D *EMf);
-
-void communicateNodeBoxStencilBC_P(int nx, int ny, int nz, arr3_double vector, int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, int bcFaceZright, int bcFaceZleft, const VirtualTopology3D * vct, EMfields3D *EMf);
-
-void communicateNodeBC_P(int nx, int ny, int nz, arr3_double vector, int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, int bcFaceZright, int bcFaceZleft, const VirtualTopology3D * vct, EMfields3D *EMf);
-
-
-
-
-// /////////// communication + BC ////////////////////////////
-void communicateCenterBC(int nx, int ny, int nz, arr3_double vector, int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, int bcFaceZright, int bcFaceZleft, const VirtualTopology3D * vct, EMfields3D *EMf);
-
-// /////////// communication + BC ////////////////////////////
-void communicateCenterBC_P(int nx, int ny, int nz, arr3_double vector, int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, int bcFaceZright, int bcFaceZleft, const VirtualTopology3D * vct, EMfields3D *EMf);
-
-/** communicate ghost cells (FOR CENTERS) with BOX stencil*/
-void communicateCenterBoxStencilBC(int nx, int ny, int nz, arr3_double vector, int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, int bcFaceZright, int bcFaceZleft, const VirtualTopology3D * vct, EMfields3D *EMf);
-
-// particles communicate ghost cells (FOR CENTERS) with BOX stencil*/
-void communicateCenterBoxStencilBC_P(int nx, int ny, int nz, arr3_double vector, int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, int bcFaceZright, int bcFaceZleft, const VirtualTopology3D * vct, EMfields3D *EMf);
-
+void communicateNode_P_old(int nx, int ny, int nz, int ns, double ****vector, const VirtualTopology3D *vct, EMfields3D *EMf);
+// void communicateInterp(int nx, int ny, int nz, double ****vector, int ns, const VirtualTopology3D * vct);
+void communicateInterp_old(int nx, int ny, int nz, int ns, double ****vector, 
+                           int bcFaceXright, int bcFaceXleft, int bcFaceYright, int bcFaceYleft, int bcFaceZright, int bcFaceZleft, 
+                           const VirtualTopology3D *vct, EMfields3D *EMf);
 
 void communicateInterp(int nx, int ny, int nz, double*** vector, const VirtualTopology3D * vct, EMfields3D *EMf);
+
 void addCorner(int nx, int ny, int nz, double ***vector,const VirtualTopology3D * vct);
 void addEdgeX(int nx, int ny, int nz, double ***vector, const VirtualTopology3D * vct);
 void addEdgeY(int nx, int ny, int nz, double ***vector, const VirtualTopology3D * vct);
